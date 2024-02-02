4e816fc1b23c702990f3f0bf469b3a8cc1745d89
Manvir Mann <72842990+mannmanvir@users.noreply.github.com>
2024-02-01 17:28:02 -0800

    * Add a check to the Notifications page to ensure that users who are not signed in cannot view notifications.

--- frontend/app/notifications/page.js
+++ frontend/app/notifications/page.js
@@ -22,6 +22,8 @@ const NotificationItem = ({ type, message }) => {
 
 export default function Notifications() {
   const [notifications, setNotifications] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const getAllNotifications = async () => {
     try {
       const res = await axios.get(`api/users/notifications`);
@@ -32,20 +34,47 @@ export default function Notifications() {
     }
   };
 
+  const checkLogin = async () => {
+    try {
+      const res = await axios.get(`api/users/isLoggedIn`, {
+        headers: { 'Cache-Control': 'no-cache' },
+      });
+      setIsLoggedIn(res.data.loggedIn);
+      return res;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
+    checkLogin();
     getAllNotifications();
   }, []);
 
   return (
     <div className="min-h-screen flex flex-col justify-between bg-gradient-to-r from-green-400 to-blue-500">
-      <div className="w-1/2 mx-auto mt-7 bg-white rounded-lg shadow-md text-center border-t border-gray-300">
-        <h1 className="text-2xl font-bold p-4">Notifications</h1>
-        <div>
-          {notifications.map((notification, index) => (
-            <NotificationItem key={index} {...notification} />
-          ))}
+      {isLoggedIn ? (
+        <div className="w-1/2 mx-auto mt-7 bg-white rounded-lg shadow-md text-center border-t border-gray-300">
+          <h1 className="text-2xl font-bold p-4">Notifications</h1>
+          <div>
+            {notifications.map((notification, index) => (
+              <NotificationItem key={index} {...notification} />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="w-1/2 mx-auto mt-7 bg-white rounded-lg shadow-md text-center border-t border-gray-300">
+          <h1 className="text-2xl font-bold p-4">
+            Please Log In to View Notifications
+          </h1>
+          <a
+            href="/login"
+            className="mb-4 bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded inline-block"
+          >
+            Login
+          </a>
+        </div>
+      )}
     </div>
   );
 }
